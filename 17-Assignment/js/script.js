701c1667f90ef7092356e287aaeec6cf3c21f74f
Vinod Chandra <vinodchandra979@gmail.com>
2024-04-01 18:33:32 +0530

    added slider funtionality

--- 17-Assignment/js/script.js
+++ 17-Assignment/js/script.js
@@ -47,6 +47,56 @@ function slidefun(n) {
 
 
 
+// Testimonial
+const slides= document.querySelectorAll('.testimonial-slide');
+let count = 1;
+slidetest(count);
+
+
+function plusSlides(n) {
+	count += n;
+	slidetest(count);
+}
+function currentSlide(n) {
+	count = n;
+	slidetest(count);
+}
+
+
+function slidetest(n) {
+	
+	let i;
+	for(i = 0;i<slides.length;i++){
+		slides[i].classList.remove("active");
+	}
+	if(n > slides.length){
+	   count = 1;
+	   }
+	if(n < 1){
+	   count = slides.length;
+	}
+	slides[count - 1].classList.add("active");
+}
+
+
+//Tab
+
+function openTab(evt, tabName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].classList.remove("active");
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].classList.remove("active");
+	}
+	// document.getElementById(tabName).style.display = "block";
+	document.getElementById(tabName).classList.add("active");
+	evt.currentTarget.classList.add("active");
+}
+
+
 // ACCORDION
 
 var acc = document.getElementsByClassName("accordion");
